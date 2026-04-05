1931b4978b7facb68526598cb88b149447228dde
Joshi <madugulajoshi011@gmail.com>
2026-04-05 14:04:15 +0530

    feat: new migrations for indexes and views for dashboard summary

---
+++ src/prisma/migrations/20260405075018_updated_index/migration.sql
@@ -0,0 +1,59 @@
+-- DropIndex
+DROP INDEX "idx_al_action";
+
+-- DropIndex
+DROP INDEX "idx_al_created_at";
+
+-- DropIndex
+DROP INDEX "idx_al_entity";
+
+-- DropIndex
+DROP INDEX "idx_al_performed_by";
+
+-- DropIndex
+DROP INDEX "idx_categories_deleted_at";
+
+-- DropIndex
+DROP INDEX "idx_categories_is_system";
+
+-- DropIndex
+DROP INDEX "idx_categories_name";
+
+-- DropIndex
+DROP INDEX "idx_fr_category_id";
+
+-- DropIndex
+DROP INDEX "idx_fr_composite";
+
+-- DropIndex
+DROP INDEX "idx_fr_created_at";
+
+-- DropIndex
+DROP INDEX "idx_fr_date";
+
+-- DropIndex
+DROP INDEX "idx_fr_deleted_at";
+
+-- DropIndex
+DROP INDEX "idx_fr_type";
+
+-- DropIndex
+DROP INDEX "idx_fr_user_id";
+
+-- DropIndex
+DROP INDEX "idx_rt_expires_at";
+
+-- DropIndex
+DROP INDEX "idx_rt_is_revoked";
+
+-- DropIndex
+DROP INDEX "idx_rt_user_id";
+
+-- DropIndex
+DROP INDEX "idx_users_deleted_at";
+
+-- DropIndex
+DROP INDEX "idx_users_email";
+
+-- DropIndex
+DROP INDEX "idx_users_role_status";
