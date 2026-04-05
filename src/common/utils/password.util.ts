24bfee1076811af972c5a2280e7d54f852522b9d
Joshi <madugulajoshi011@gmail.com>
2026-04-05 20:09:22 +0530

    fix: bug fix for password hashing

--- src/common/utils/password.util.ts
+++ src/common/utils/password.util.ts
@@ -1,10 +1,12 @@
 import bcrypt from 'bcrypt';
 import { env } from '../../config/env';
 
+const saltRounds = parseInt(env.BCRYPT_SALT_ROUNDS, 10);
+
 export const PasswordUtil = {
 
   async hash(plainText: string): Promise<string> {
-    return bcrypt.hash(plainText, env.BCRYPT_SALT_ROUNDS);
+    return bcrypt.hash(plainText, saltRounds);
   },
 
   async verify(plainText: string, hash: string): Promise<boolean> {
